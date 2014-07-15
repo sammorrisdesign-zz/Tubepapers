8f212eee6052d0bb7e97128f833422462b696f66
Sam Morris <mail@sammorrisdesign.co.uk>
2014-07-15 08:54:33 +0100

    Use data-event to find the most popular patterns

--- js/main.js
+++ js/main.js
@@ -2,25 +2,29 @@
 var favicons = ["favicon_bakerloo.png", "favicon_barman.png", "favicon_central.png", "favicon_district.png", "favicon_hammer.png", "favicon_metropolitian.png", "favicon_overground.png", "favicon_victoria.png", "favicon_waterloo.png"];
 $(".favicon").attr("href", "assets/images/" + favicons[Math.floor(Math.random() * favicons.length)]);
 
+// GoSquared Event Logging
+$('[data-event]').on('click', function() {
+    _gs('event', $(this).data('event'));
+});
 
 $(window).load(function() {
-	// Go for animations
-	$("body").removeClass("preload");
-	$(".main__title").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function(){
-		$(this).css("opacity", 1);
-	});
-	
-	$(".main__element:last-of-type .main__title").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function(){
-		$("body").addClass("finished");
-	});
+    // Go for animations
+    $("body").removeClass("preload");
+    $(".main__title").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function(){
+        $(this).css("opacity", 1);
+    });
+    
+    $(".main__element:last-of-type .main__title").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function(){
+        $("body").addClass("finished");
+    });
 
-	// Click
-	$(".main__title").click(function() {
-		if ($(this).siblings(".main__links").hasClass("main__links--visible")) {
-			$(this).siblings(".main__links").removeClass("main__links--visible");
-		} else {
-			$(".main__links--visible").removeClass("main__links--visible");
-			$(this).siblings(".main__links").addClass("main__links--visible");
-		}
-	});
+    // Click
+    $(".main__title").click(function() {
+        if ($(this).siblings(".main__links").hasClass("main__links--visible")) {
+            $(this).siblings(".main__links").removeClass("main__links--visible");
+        } else {
+            $(".main__links--visible").removeClass("main__links--visible");
+            $(this).siblings(".main__links").addClass("main__links--visible");
+        }
+    });
 });
